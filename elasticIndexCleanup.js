409ed099274ad96ac1316ef811f6650d54c38208
cian-chambliss <cian@alphasoftware.com>
2015-11-19 15:58:32 -0500

    Helper to cleanup orphans in elastic search index (removes all keys that don't currently point at pages)

---
+++ elasticIndexCleanup.js
@@ -0,0 +1,38 @@
+module.exports = function (settings, basePath) {
+	var fs = require('fs');
+	var async = require('async');
+	var elasticsearch = require('elasticsearch');
+	var client = new elasticsearch.Client({ host: (settings.host || 'localhost:9200') });
+	var queryDef = { "match_all": {} };
+	client.search({
+		index: (settings.index || 'helpserver'),
+		body: {
+			from: 0,
+			size: 100000,
+			query: queryDef,
+			_source: ["title", "path"]
+		}
+	}, function (error, response) {
+		if (error) {
+			console.log("error:" + error);
+		} else {
+			async.eachSeries(response.hits.hits, function (fo, callbackLoop) {
+				var pathPart = fo._source.path.split('#')[0];
+				fs.exists(basePath + pathPart, function (exists) {
+					if (!exists) {
+						client.delete({
+							index: (settings.index || 'helpserver'),
+							type: (settings.type || "helppage"),
+							id: fo._source.path,
+						}, function (error, response) {
+							console.log('remove reference ' + fo._source.path);
+							callbackLoop();
+						});
+					} else {
+						callbackLoop();
+					}
+				});
+			});
+		}
+	});
+}
