ca8332e62b6b9f97807dfec9f834b7b063a3e965
cian-chambliss <cian@alphasoftware.com>
2016-06-18 14:45:42 -0400

    Added handling of multiple Git repos

--- updatesource.js
+++ updatesource.js
@@ -2,27 +2,53 @@
  * Incrementally update the search index (plain text) for the help system
  */
 module.exports = function (config, callback) {
-   var nodegit = require('nodegit');
-   var repository;
-   nodegit.Repository.open(config.repoSource ? config.repoSource : config.source )
-      .then(function (repo) {
-      repository = repo;
-      return repository.fetchAll({
-         credentials: function (url, userName) {
-            return nodegit.Cred.sshKeyFromAgent(userName);
-         },
-         certificateCheck: function () {
-            return 1;
-         }
-      });
-   })
-      .then(function () {
-      return repository.mergeBranches("master", "origin/master");
-   })
-      .catch(function (err) {
-       callback(err,false);      
-   })
-      .done(function () {
-      callback(null,true);
+   var async = require('async');
+   var reposList = null;
+   var gitSucceeded = false;
+
+   if( config.repoSource ) {
+       if( Array.isArray(config.repoSource) ) {
+           reposList = config.repoSource;
+       } else {
+           reposList = [ config.repoSource ];
+       }
+   } else {
+       reposList = [config.source ];
+   }
+   async.eachSeries(reposList, function (repoName, callbackLoop) {
+        var nodegit = require('nodegit');
+        var repository;
+        console.log("git pull "+repoName);
+        nodegit.Repository.open(repoName )
+            .then(function (repo) {
+            repository = repo;
+            return repository.fetchAll({
+                credentials: function (url, userName) {
+                    return nodegit.Cred.sshKeyFromAgent(userName);
+                },
+                certificateCheck: function () {
+                    return 1;
+                }
+            });
+        })
+            .then(function () {
+            return repository.mergeBranches("master", "origin/master");
+        })
+            .catch(function (err) {
+                console.log("Error on pull "+err);
+            if( callbackLoop ) {
+                callbackLoop();
+                callbackLoop = null;      
+            }
+        })
+            .done(function () {
+                gitSucceeded = true;
+            if( callbackLoop ) {
+                callbackLoop();
+                callbackLoop = null;      
+            }
+    });
+   },function() {
+       callback(null,gitSucceeded);
    });
 }
